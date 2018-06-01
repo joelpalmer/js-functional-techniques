d6867281da22b8cd436b2e154f44a22d4fffb590
Joel Palmer <joelpalmer@gmail.com>
2018-06-01 09:48:59 -0500

    init commit

---
+++ const.js
@@ -0,0 +1,16 @@
+/*
+    const 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+
+    const is used to declare a block scoped local
+    
+    const is the only keyword for declaring variables that should be used in JS FP
+
+    You can try to be disciplined or just enforce this: https://github.com/jfmengels/eslint-plugin-fp
+    eslint rules: 
+    "fp/no-let": "error",
+    "no-var": "error"
+*/
+
+const x = "You can't re-assign me.";
+x;

214ec708d843a8a0950515be8012410443d62cde
Joel Palmer <joelpalmer@gmail.com>
2018-06-01 10:42:57 -0500

    add not() and placeholders for other decorators

---
+++ decorators/not.js
@@ -0,0 +1,14 @@
+/* 
+    * → Boolean
+    
+    FP is not about being fancy. It's actually about
+    simplifying code and making it more readable.
+
+    
+*/
+
+const not = a => !a;
+
+const x = not(true);
+x;
+

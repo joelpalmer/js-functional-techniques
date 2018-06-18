1d6a8a963f7ea6c577b16880c04597b72c2348d5
Joel Palmer <joelpalmer@gmail.com>
2018-06-18 12:56:01 -0500

    implement and test all()

--- Ramda/List/all.js
+++ Ramda/List/all.js
@@ -6,8 +6,7 @@
 import _dispatchable from "ramda/src/internal/_dispatchable";
 import _curry2 from "ramda/src/internal/_curry2";
 import _xall from "ramda/src/internal/_xall";
-import {toUpper, all} from 'ramda';
-
+import { equals } from "ramda";
 /* 
     Ramda implements _dispatchable on all(). That is to say that if
     you apply it to an array-like object & that object has an any() method,
@@ -16,7 +15,32 @@ import {toUpper, all} from 'ramda';
     a use case for that, maybe they're future proofing or maybe I am misunderstanding
     In any case, if you add an all() to Array.prototype, it should get used
 */
+//todo: decide if I should dig in to transducers now or wait until R.transduce
+//implement all()
 
+// might be interesting to try dispatchable with ['every']
+// Note: tried it with every and it appeared to work. So, tried it with
+// others and it didn't break it. Needs further investigation 
+const all = _curry2(_dispatchable(['all'], _xall, function all(fn, list) {
+    let idx = 0;
+    while (idx < list.length) {
+        if (!fn(list[idx])) {
+            return false;
+        }
+        idx += 1;
+    }
+    return true;
+}));
+
+//let's try this
+const j = equals("Joel");
+const x = all(j)(["Joel", "Joel", "Joel"]);
+const y = all(j)(["Joe", "Joel", "Joel"]);
+//But, can I do this...
+const z = all(j, ["Joel", "Joel"]); //yep, it shouldn't and doesn't matter
+x;
+y;
+z;
 
 
 
